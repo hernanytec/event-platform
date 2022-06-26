d5a0cf5c4ff71533556df2330d87c44eeee34d3c
hernany <hernanytec777@gmail.com>
2022-06-26 20:11:22 -0300

    feat: add subscribe page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: import.meta.env.VITE_HEADLESS_CMS_URI,
+  uri: import.meta.env.VITE_GRAPH_CMS_URI,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_GRAPH_CMS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
